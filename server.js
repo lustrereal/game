df8c5b7207fe4a2cc6a56aad5f460e4836ad0afd
lustrereal <lustretemp@gmail.com>
2026-02-26 13:44:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// Updated server.js (full file - copy-paste replace)
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static(__dirname)); // Serve client files
 
-const players = {}; // Store player data: {id: {x, y, color}}
+const players = {}; // {id: {x, y, color, name}}
 
 io.on('connection', (socket) => {
   console.log('A player connected:', socket.id);
@@ -17,10 +23,13 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newPlayer', { id: socket.id });
 
   socket.on('join', (data) => {
+    if (!data.name || data.name.trim().length === 0) data.name = 'Anonymous';
+    data.name = data.name.trim().substring(0, 16);
     players[socket.id] = {
-      x: Math.floor(Math.random() * 800) + 50, // Random start position
+      x: Math.floor(Math.random() * 800) + 50,
       y: Math.floor(Math.random() * 600) + 50,
-      color: data.color
+      color: data.color,
+      name: data.name
     };
     io.emit('playerJoined', { id: socket.id, ...players[socket.id] });
   });
@@ -29,7 +38,20 @@ io.on('connection', (socket) => {
     if (players[socket.id]) {
       players[socket.id].x = movementData.x;
       players[socket.id].y = movementData.y;
-      socket.broadcast.emit('playerMoved', { id: socket.id, ...movementData });
+      socket.broadcast.emit('playerMoved', { id: socket.id, x: movementData.x, y: movementData.y });
+    }
+  });
+
+  socket.on('chatMessage', (data) => {
+    if (players[socket.id] && data.message && data.message.trim()) {
+      const player = players[socket.id];
+      const message = data.message.trim().substring(0, 100);
+      io.emit('chatMessage', {
+        id: socket.id,
+        name: player.name,
+        color: player.color,
+        message: message
+      });
     }
   });
 
